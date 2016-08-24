d0a1679db306f89c27bab0e1ec01bacf3f02fb5f
S Vogel <doctorjdk@yahoo.com>
2016-08-24 17:58:30 +0000

    Lab1 folder with ASCII Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout<<"---    ---"<<endl;
+cout<<"'O'    'O'"<<endl;
+cout<<"    L"<<endl;     
+cout<<"^       ^"<<endl;
+cout<<"[_______]"<<endl;    
+}
