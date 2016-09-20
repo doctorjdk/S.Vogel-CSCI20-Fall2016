d02581e33e13b87dbf09fc06f2196b01e551b558
S Vogel <doctorjdk@yahoo.com>
2016-09-20 21:17:13 +0000

    Lab7 Song

---
+++ lab7/lab7.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void Bridge() {
+    
+    cout << "How will I know (Don't trust your feelings)" << endl;
+    cout << "How will I know" << endl;
+    cout << "How will I know (Love can be deceiving)" << endl;
+    cout << "How will I know" << endl << endl;
+  
+    return;
+}
+
+void Chorus() {
+    cout << "How will I know if he really loves me" << endl;
+    cout << "I say a prayer with every heartbeat" << endl;
+    cout << "I fall in love whenever we meet" << endl;
+    cout << "I'm asking you what you know about these things" << endl;
+    cout << "How will I know if he's thinking of me" << endl;
+    cout << "I try to phone but I'm too shy (can't speak)" << endl;
+    cout << "Falling in love is so bittersweet" << endl;
+    cout << "This love is strong why do I feel weak" << endl << endl;
+    
+    return;
+}
+
+int main () {
+    
+    cout << "There's a boy I know, he's the one I dream of" << endl;
+    cout << "Looks into my eyes, takes me to the clouds above, mm hmm" << endl;
+    cout << "Oh I lose control, can't seem to get enough, uh huh" << endl;
+    cout << "When I wake from dreaming, tell me is it really love uh oh" << endl << endl;
+    
+    Bridge();
+    Chorus();
+    
+    cout << "Oh wake me, I'm shaking, wish I had you near me now, uh huh" << endl;
+    cout << "Said there's no mistaking, what I feel is really love, oh tell me" << endl;
+    
+    Bridge();
+    Chorus();
+    
+    cout << "If he loves me, if he loves me not" << endl;
+    cout << "If he loves me, if he loves me not" << endl;
+    cout << "If he loves me, if he loves me not" << endl << endl;
+    
+    cout << "How will I know (repeat 5x)" << endl << endl;
+    
+    Chorus();
+    
+    cout << "How will I know (repeat)" << endl;
+    
+    
+    
+    
+}
