98d40b9c55234b87130288dc8f81c0a7059129dc
S Vogel <doctorjdk@yahoo.com>
2016-10-05 01:38:49 +0000

    lab9 Temperature Conversions

--- lab4/test.cpp
+++ lab4/test.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
-
-double VolumeBox(double length, double width, double height)
+void SwapVectorEnds (vector<int>& sortVector)
 {
-    double BoxVol;
-    BoxVol = length * width * height;
-    return BoxVol;
+    
+    int i = 0;
+    int tempVal = 0;
+    
+    for (i = 0; i < sortVector.size(); ++i) {
+        
+        tempVal = sortVector.at(i);
+        sortVector.at(0) = sortVector.at(sortVector.size()-1);
+        sortVector.at(sortVector.size()-1) = tempVal;
+        
+    }
+    
+    return;
 }
 
-int main() {
+int main () 
+{
+    vector<int> sortVector(4);
+    int i = 0;
     
-    cout << VolumeBox(3,1,2*5) << " meters squared." << endl;
+    sortVector.at(0) = 10;
+    sortVector.at(1) = 20;
+    sortVector.at(2) = 30;
+    sortVector.at(3) = 40;
+    
+    
+    SwapVectorEnds(sortVector);
+    
+    for (i = 0; i <sortVector.size(); ++i){
+        cout << sortVector.at(i) << " ";
+    }
+    
+    cout << endl;
+    return 0;
 }
